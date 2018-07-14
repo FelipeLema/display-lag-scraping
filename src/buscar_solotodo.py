33f88c6fe382e724546a28bf9b5b7287a6ddd60e
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2018-07-14 15:52:24 -0400

    (incompleto) Renovando lector solotodo

--- src/buscar_solotodo.py
+++ src/buscar_solotodo.py
@@ -14,24 +14,24 @@ class SolotodoParser(HTMLParser):
     def __init__(self):
         super(SolotodoParser, self).__init__()
         self.resultados = []
-        self.mirandoEntradaDeResultado = False
 
     def handle_starttag(self, tag, attrs):
         _attrs = defaultdict(lambda: None)
         for k,v in attrs:
             _attrs[k] = v
 
-        if tag == "h4" and _attrs["class"] == "search-result-title":
-            self.mirandoEntradaDeResultado = True
+        if tag != "a":
+            return
+        if "href" not in _attrs.keys():
+            return
+        if not _attrs["href"].startswith('/products/'):
             return
 
-        if self.mirandoEntradaDeResultado and tag == "a":
-            self.resultados.append(_attrs["href"])
-            return
+        self.resultados.append(_attrs["href"])
+        return
             
     def handle_endtag(self, tag):
-        if tag == "div":
-            self.mirandoEntradaDeResultado = False
+        pass
 
     def handle_data(self, data):
         pass
@@ -44,22 +44,24 @@ def buscar_solotodo(consulta=None, archHtml=None,
     '''
     if archHtml is None:
         assert(consulta is not None)
-        fileName, headers = urllib.request.urlretrieve(
-            "http://www.solotodo.com/search/?keywords={}".format(consulta))
+        request = urllib.request.Request(
+            "https://www.solotodo.com/search?search={}".format(consulta),
+            headers={'User-Agent': 'Mozilla/5.0'})
+        htmlStr = urllib.request.urlopen(request).read().decode(encoding='utf-8')
     else:
         assert(consulta is None)
-        fileName = archHtml
+        with open(archHtml) as htmlFile:
+            htmlStr = '\n'.join(htmlFile.readlines())
 
+    raise RuntimeError("Aprender a ocupar selenium")
     parser = SolotodoParser()
-    with open(fileName) as htmlFile:
-        htmlStr = '\n'.join(htmlFile.readlines())
-        parser.feed(htmlStr)
-        if not parser.resultados and avisarVacíos:
-            nombreRegistro = "./última_consulta-{0}.html".format(
-                datetime.datetime.now().isoformat())
-            with open(nombreRegistro, "w") as registro:
-                registro.write(htmlStr)
-            warnings.warn("Sin resultados, revisa {}".format(nombreRegistro))
+    parser.feed(htmlStr)
+    if not parser.resultados and avisarVacíos:
+        nombreRegistro = "./última_consulta-{0}.html".format(
+            datetime.datetime.now().isoformat())
+        with open(nombreRegistro, "w") as registro:
+            registro.write(htmlStr)
+        warnings.warn("Sin resultados, revisa {}".format(nombreRegistro))
     resultadosÚnicos = list(set(parser.resultados))
     urlsCompletas = list(map(lambda url: "http://www.solotodo.com"+url,
                              resultadosÚnicos))
